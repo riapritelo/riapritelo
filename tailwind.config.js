f0d7b89e32bd9f3dd585f6903d980e00291616db
leonardo <leofiore@libero.it>
2021-01-25 23:13:50 +0100

    refactor: fork and steal from existing boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   theme: {
-    // Some useful comment
+      extend: {
+        backgroundImage: theme => ({
+         'tra-white': "url('/src/trabianco.svg')",
+         'tra-black': "url('/src/tranero.svg')",
+        })
+      }
   },
   variants: {
     // Some useful comment
